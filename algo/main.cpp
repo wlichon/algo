a3524d306e4193cd29875dda11fa7501cf187873
zerleger <101527159+zerleger@users.noreply.github.com>
2022-04-15 08:08:54 +0200

    finished

--- algo/main.cpp
+++ algo/main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include "Tree.h"
+#include "Node.h"
 
-int main() {
-	std::cout << "hello world\n";
+using namespace std;
+
+int main(int argc, char** argv) {
+	Tree tree;
+	if (argc == 2) {
+		char* searchTree = argv[1];
+		tree.treeRead(searchTree);
+	}
+	else if (argc == 3) {
+		char* searchTree = argv[1];
+		char* subTree = argv[2];
+		tree.treeRead(searchTree,subTree);
+	}
+
+	
 }
